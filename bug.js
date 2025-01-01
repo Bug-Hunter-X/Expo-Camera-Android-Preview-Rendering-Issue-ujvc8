0e3e746cf092372236eef66a24a8a4854bce7ab6
BugHunter-X <askzenai@gmail.com>
2025-01-01 12:42:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo Camera API with a custom camera component. The preview doesn't render correctly on Android devices, showing a blank screen or a distorted image.  The issue seems specific to certain Android versions and devices. iOS functions correctly.
